8ea0a50b9615c33f4dae2b9297741c4366cab752
idetachment2702 <idetachment2702@gmail.com>
2025-11-11 04:14:17 +0900

    Updates

---
+++ assets/2022-05-31_FlexiSpot-E8-CdjuWmTC.js
@@ -0,0 +1,79 @@
+const a=`---
+title: FlexiSpot 電動昇降デスク E8
+tags: [デスク]
+---
+
+<a href="https://www.flexispot.jp/e8-bamboo.html?gad_source=1&gad_campaignid=21400905724&gbraid=0AAAAACUbSSTpZs6pcF5pkLVCSkfqXu4h6&gclid=CjwKCAiAlMHIBhAcEiwAZhZBUkrRuCe-X-VCUbk-1tV0JX6C-1eaFoewdESj44jelR5dQAiAOyUeJRoCPmwQAvD_BwE" class="link-card" target="_blank" rel="noopener noreferrer">
+  <div class="link-card-content">
+    <div class="link-card-text">
+      <div class="link-card-title">電動昇降式デスク・E8 | FlexiSpot 公式ストア</div>
+      <div class="link-card-description"> 安全を考慮し、脚部の支柱は丸みを持たせた楕円形に仕上げました。環境負荷の低いFlexiSpot昇降デスクE8を活用し、健康と持続可能な社会生活を心がけています。</div>
+      <div class="link-card-url">
+        <img class="link-card-favicon" src="https://www.flexispot.jp/favicon.ico" alt="" />
+        <span>www.flexispot.jp</span>
+      </div>
+    </div>
+    <div class="link-card-image"><img src="https://image.springbeetle.asia/cdn-cgi/image/dpr=1,format=webp,fit=pad/https://cnmegk4mhxmt.compat.objectstorage.ap-tokyo-1.oraclecloud.com/asia/trantor/attachments/JP/e8white-c1407bamboo-800-1_1.jpg" alt="電動昇降式デスク・E8 | FlexiSpot 公式ストア" /></div>
+  </div>
+</a>
+
+座りっぱなしでいると股がと腰への負担が大きい……という自覚がめばえたため、スタンディングデスクの導入を決めた
+
+アクセサリーもいくつか購入したのだが、どれも自分の環境だとあわなかったため
+
+<a href="https://www.flexispot.jp/s02.html" class="link-card" target="_blank" rel="noopener noreferrer">
+  <div class="link-card-content">
+    <div class="link-card-text">
+      <div class="link-card-title">卓下式キャビネット・S02 | FlexiSpot 公式ストア</div>
+      <div class="link-card-description"> FlexiSpot 卓下式キャビネット S02 簡単に取り付け ダイヤル錠 大容量収納</div>
+      <div class="link-card-url">
+        <img class="link-card-favicon" src="https://www.flexispot.jp/favicon.ico" alt="" />
+        <span>www.flexispot.jp</span>
+      </div>
+    </div>
+    <div class="link-card-image"><img src="https://image.springbeetle.asia/cdn-cgi/image/dpr=1,format=webp,fit=pad/https://cnmegk4mhxmt.compat.objectstorage.ap-tokyo-1.oraclecloud.com/asia/trantor/attachments/JP/s02-b-800.png" alt="卓下式キャビネット・S02 | FlexiSpot 公式ストア" /></div>
+  </div>
+</a>
+
+卓下キャビネットも買ってみたのだが、これはかなり大きいため脇にあると何度も脚をぶつけるのでよくなかった。
+
+これはH型のフレームを天板につける都合上、天板の端には袖机をつけられない、というのが理由としては大きい。
+
+中身も大きさにみあわない上下2段、しかも側面が湾曲しているので卓上引き出しのようなものを入れて解決、というのも難しかった。
+
+結局小型のキッチンワゴンや収納箱、デスクシェルフの引き出しで解決することになった。
+
+<a href="https://www.flexispot.jp/cmp502.html" class="link-card" target="_blank" rel="noopener noreferrer">
+  <div class="link-card-content">
+    <div class="link-card-text">
+      <div class="link-card-title">ケーブルトレー・CMP502 | FlexiSpot 公式ストア</div>
+      <div class="link-card-description"> FlexiSpot ケーブルトレーCMP502はデスクに直接取り付けるタイプで、デスク下の空間をよくご利用できます。</div>
+      <div class="link-card-url">
+        <img class="link-card-favicon" src="https://www.flexispot.jp/favicon.ico" alt="" />
+        <span>www.flexispot.jp</span>
+      </div>
+    </div>
+    <div class="link-card-image"><img src="https://image.springbeetle.asia/cdn-cgi/image/dpr=1,format=webp,fit=pad/https://cnmegk4mhxmt.compat.objectstorage.ap-tokyo-1.oraclecloud.com/asia/trantor/attachments/JP/CMP502B-800-2112.png" alt="ケーブルトレー・CMP502 | FlexiSpot 公式ストア" /></div>
+  </div>
+</a>
+
+ケーブルトレーはほぼ必須なのだが、FlexiSpotのものは高さが足りないので電源タップを入れるとキツイ。
+
+↓の大型トレーが見た目はゴツいが、配線のストレスが少ない。
+
+<a href="https://www.amazon.co.jp/dp/B0CBLVSC3Q" class="link-card" target="_blank" rel="noopener noreferrer">
+<div class="link-card-content">
+<div class="link-card-text">
+<div class="link-card-title">Amazon｜山崎実業(Yamazaki) デスク下 天板ケーブルラック ロング ブラック 約W63.5×D12×H17cm タワー tower ケーブル 電源タップ 収納 ネジで固定 カバー付き 4479｜収納用品 オンライン通販</div>
+<div class="link-card-description">山崎実業(Yamazaki) デスク下 天板ケーブルラック ロング ブラック 約W63.5×D12×H17cm タワー tower ケーブル 電源タップ 収納 ネジで固定 カバー付き 4479が収納用品ストアでいつでもお買い得。当日お急ぎ便対象商品は、当日お届け可能です。アマゾン配送商品は、通常配送無料（一部除く）。</div>
+<div class="link-card-url">
+<img class="link-card-favicon" src="https://www.amazon.co.jp/favicon.ico" alt="" />
+<span>www.amazon.co.jp</span>
+</div>
+</div>
+<div class="link-card-image">
+<img src="https://m.media-amazon.com/images/I/61HLpix4yPL._AC_SX300_SY300_QL70_ML2_.jpg" alt="Amazon｜山崎実業(Yamazaki) デスク下 天板ケーブルラック ロング ブラック 約W63.5×D12×H17cm タワー tower ケーブル 電源タップ 収納 ネジで固定 カバー付き 4479｜収納用品 オンライン通販" />
+</div>
+</div>
+</a>
+`;export{a as default};
---
+++ assets/2025-11-04_エアージャッキ-DGR0quPf.js
@@ -0,0 +1,31 @@
+const n=`---
+title: エアージャッキ
+tags: [エアージャッキ, デスク]
+---
+
+<a href="https://www.amazon.co.jp/dp/B0F1KC958V" class="link-card" target="_blank" rel="noopener noreferrer">
+<div class="link-card-content">
+<div class="link-card-text">
+<div class="link-card-title">Amazon.co.jp: LeTradeJP エアージャッキ エアーウエッジ 2個セット 強度TPU製品 150KG対応 ジャッキアップ ドアロック 解除 手動式 補助工具 : 産業・研究開発用品</div>
+<div class="link-card-description">Amazon.co.jp: LeTradeJP エアージャッキ エアーウエッジ 2個セット 強度TPU製品 150KG対応 ジャッキアップ ドアロック 解除 手動式 補助工具 : 産業・研究開発用品</div>
+<div class="link-card-url">
+<img class="link-card-favicon" src="https://www.amazon.co.jp/favicon.ico" alt="" />
+<span>www.amazon.co.jp</span>
+</div>
+</div>
+<div class="link-card-image">
+<img src="https://m.media-amazon.com/images/G/09/gno/sprites/nav-sprite-global-1x-reorg-privacy._CB546377955_.png" alt="Amazon.co.jp: LeTradeJP エアージャッキ エアーウエッジ 2個セット 強度TPU製品 150KG対応 ジャッキアップ ドアロック 解除 手動式 補助工具 : 産業・研究開発用品" />
+</div>
+</div>
+</a>
+
+[電動スタンディングデスク](/article/2022-05-31_FlexiSpot-E8)はかなりの重量物なので、これまで苦労して扱っていた。
+
+特に足の下に猫砂が入ってしまうことが頻発していて、たまに持ち上げて掃除するタスクが発生している。
+
+そこでこのエアージャッキを導入。
+
+使い方はシンプルで、平たいジャッキアップポイントを足の下にすべりこませ、ポンプをシュコシュコするだけ。
+
+簡単に足が持ち上がる。足の下にしいていたマットを取り除くにも重宝した。
+`;export{n as default};
